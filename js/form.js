b23bb8c01b40640b252ff88834cf32795e60daab
Vasilis Tsaliagos <billakos.tsaliagos@gmail.com>
2023-01-14 19:05:40 +0200

    e commit

--- js/form.js
+++ js/form.js
@@ -1,74 +1,74 @@
-const form = [...document.querySelector('.form').children];
+// const form = [...document.querySelector('.form').children];
 
-form.forEach((item, i) => {
-    setTimeout(() => {
-        item.style.opacity = 1;
-    }, i*100);
-});
+// form.forEach((item, i) => {
+//     setTimeout(() => {
+//         item.style.opacity = 1;
+//     }, i*100);
+// });
 
-window.onload = () => {
-    if(sessionStorage.name){
-        location.href = '/';
-    }
-}; 
+// window.onload = () => {
+//     if(sessionStorage.name){
+//         location.href = '/';
+//     }
+// }; 
 
-const name = document.querySelector('.name') || null;
-const email = document.querySelector('.email');
-const password = document.querySelector('.password');
-const submitBtn = document.querySelector('.submit-btn');
+// const name = document.querySelector('.name') || null;
+// const email = document.querySelector('.email');
+// const password = document.querySelector('.password');
+// const submitBtn = document.querySelector('.submit-btn');
 
-if(name == null){
-    submitBtn.addEventListener('click', () => {
-        fetch('/login-user',{
-            method: 'post',
-            headers: new Headers({'Content-Type': 'application/json'}),
-            body: JSON.stringify({
-                email: email.value,
-                password: password.value
-            })
-        })
-        .then(res => res.json())
-        .then(data => {
-            validateData(data);
-        })
-    });
-} else{
-    submitBtn.addEventListener('click', () => {
-        fetch('/register-user', {
-            method: 'post',
-            headers: new Headers({'Content-Type': 'application/json'}),
-            body: JSON.stringify({
-                name: name.value,
-                email: email.value,
-                password: password.value
-            })
-        })
-        .then(res => res.json())
-        .then(data => {
-            validateData(data);
-        })
-    });
-}
+// if(name == null){
+//     submitBtn.addEventListener('click', () => {
+//         fetch('/login-user',{
+//             method: 'post',
+//             headers: new Headers({'Content-Type': 'application/json'}),
+//             body: JSON.stringify({
+//                 email: email.value,
+//                 password: password.value
+//             })
+//         })
+//         .then(res => res.json())
+//         .then(data => {
+//             validateData(data);
+//         })
+//     });
+// } else{
+//     submitBtn.addEventListener('click', () => {
+//         fetch('/register-user', {
+//             method: 'post',
+//             headers: new Headers({'Content-Type': 'application/json'}),
+//             body: JSON.stringify({
+//                 name: name.value,
+//                 email: email.value,
+//                 password: password.value
+//             })
+//         })
+//         .then(res => res.json())
+//         .then(data => {
+//             validateData(data);
+//         })
+//     });
+// }
 
-const validateData = (data) => {
-    if(!data.name){
-        alertBox(data);
-    } else{
-        sessionStorage.name = data.name;
-        sessionStorage.email = data.email;
-        location.href = '/';
-    }
-}
+// const validateData = (data) => {
+//     if(!data.name){
+//         alertBox(data);
+//     } else{
+//         sessionStorage.name = data.name;
+//         sessionStorage.email = data.email;
+//         location.href = '/';
+//     }
+// }
 
 
-    const alertContainer = document.querySelector('.alert-box');
-    const alertMsg = document.querySelector('.alert');
-    alertMsg.innerHTML = data;
+//     const alertContainer = document.querySelector('.alert-box');
+//     const alertMsg = document.querySelector('.alert');
+//     alertMsg.innerHTML = data;
     
-    alertContainer.style.top = `5%`;
-    setTimeout(() => {
-        alertContainer.style.top = null;
-    }, 5000);
+//     alertContainer.style.top = `5%`;
+//     setTimeout(() => {
+//         alertContainer.style.top = null;
+//     }, 5000);
     
 
 // // form loading animation
